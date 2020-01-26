2d85f98661980a1965fa99c141ef0808fb393624
Todor Todorov <ttodorov@null.net>
2020-01-26 16:00:04 +0200

    Docs: document/comment code
    Done.

--- src/win/win.c
+++ src/win/win.c
@@ -1,236 +1,323 @@
+/*
+ * The ncurses chat window object
+ * Reads its data from the irclsd output log file and displays it
+ * on the screen in 'tail -f' fashion.
+ * There is no buffer history, the 'part', 'join', 'quit' messages are not
+ * displayed. Only privmsg are displayed.
+ * Feature: when 'To:' is sent by user, all other messages which do not
+ * corespond to 'To:' are filtered
+ */
 #include <win/win.h>
-#include <global/global.h>
-#include <index/index.h>
-#include <ircproto/ircproto.h>
-#include <stylerot/stylerot.h>
+#include <global/global.h>                       //NICLEN, etc
+#include <index/index.h>                         //log file indexing
+#include <ircproto/ircproto.h>                   //irc protocol parsing
+#include <stylerot/stylerot.h>                   //rotational styles
 
 #include <ncursesw/curses.h>
-#include <stdio.h> //fopen()
-#include <wchar.h> //wchar_t
-#include <string.h> //strcspn()
-#include <stdlib.h> //srand()
-#include <time.h> //time()
-#include <sys/select.h> //select()
+#include <stdio.h>                               //fopen()
+#include <wchar.h>                               //wchar_t
+#include <string.h>                              //strcspn()
+#include <stdlib.h>                              //srand()
+#include <time.h>                                //time()
+#include <sys/select.h>                          //select()
 
 
-/*win data        fd    win   pan   read_bytes display_bytes current_row_style*/
-static Win win; //TODO: initialization here
+/*win data        fd    win   pan   index read_bytes display_bytes ircproto*/
+static Win win = {NULL, NULL, NULL, NULL, 0        , 0           , NULL};
 
+/*
+ * These are coming from global.{h, c}
+ */
 extern Style style;
 extern int count_tome;
 extern wchar_t mynick[NICKLEN + 1];
 extern wchar_t tosend[CHANLEN + 1];
 
-
+/*
+ * Initializes the chat window object
+ * filename: the filename of the irclsd output log file
+ * Returns: 0 OK, negative ERR
+ */
 int
 win_init(const char filename[PATHLEN + 1])
 {
-	int h;
-	int w;
+	int h;                                   //height
+	int w;                                   //width
 
 	getmaxyx(stdscr, h, w);
 
-	win.win = newwin(h - 1, w, 0, 0);
+	win.win = newwin(h - 1, w, 0, 0);        //create, position window
 	idlok(win.win, TRUE);                    //needed with scrollok (man)
-	scrollok(win.win, TRUE);
-	win.pan = new_panel(win.win);
+	scrollok(win.win, TRUE);                 //auto scrolling
+	win.pan = new_panel(win.win);            //own panel for chat window
 
-	win.ircproto = ircproto_new();
+	win.ircproto = ircproto_new();           //irc protocol object
 	if(win.ircproto == NULL)
 	{
 		win_del();
 		return -3;
 	}
 
-	win.fd = fopen(filename, "r");
+	win.fd = fopen(filename, "r");           //open irclsd output log file
 	if(win.fd == NULL)
 	{
 		win_del();
 		return -1;
 	}
 
-	win.index = index_new(HISTORYBUF);
+	win.index = index_new(HISTORYBUF);       //file indexing object
 	if(win.index == NULL)
 	{
 		win_del();
 		return -2;
 	}
-	win_index_file();
+	win_index_file();                        //index file for first time
 
+	/*
+	 * this emulates the last log file lines as already displayed
+	 * e.g in the future we will display only the newly logged lines
+	 * (behaviour similar to tail -f)
+	 */
 	index_get(win.index, &(win.display_bytes), -1);
 
-	return 0;
+	return 0;                                //OK
 } /*win_init()*/
 
 
+/*
+ * Frees memory and pointers
+ */
 void
 win_del(void)
 {
+	/*these are protected - if not initialized, do not delete*/
 	if(win.win != NULL) delwin(win.win);
 	if(win.pan != NULL) del_panel(win.pan);
 	if(win.fd != NULL) fclose(win.fd);
+
+	/*these are unprotected - they have their own protection inside*/
 	index_del(win.index);
 	ircproto_del(win.ircproto);
 } /*win_del()*/
 
 
+/*
+ * Draws the window on the screen
+ */
 void
 win_draw(void)
 {
-	int i;
-	char buf[CBUFLEN + 1];
-	wchar_t wbuf[WBUFLEN + 1];
-	size_t start;
-	size_t end;
-	int styl;
+	int i;                                   //index
+	char buf[CBUFLEN + 1];                   //character buffer for one ln
+	wchar_t wbuf[WBUFLEN + 1];               //wide char buf 1 ln
+	size_t start;                            //the current ln start
+	size_t end;                              //the current ln end
+	int styl;                                //ncurses style
 
-	win_index_file();
+	win_index_file();                        //index the newly received
+	                                         //lines from irclsd
 
+	/* get the previous newline bytes position in the file*/
 	i = -1;
 	index_get(win.index, &start, i);
+	/* if already displayed - return and do not display anything*/
 	if(start <= win.display_bytes) return;
 
+	/* rewind the file backwards until the last line read*/
 	i = 0;
 	do
 	{
 		if(!index_get(win.index, &start, --i)) break;
 	}
 	while(start > win.display_bytes);
+	/*now we have the first index to read in variable i*/
 	
 
+	/*display the newly received lines from irclsd*/
 	while(i < -1)
 	{
+		/*get the start byte*/
 		if(!index_get(win.index, &start, i++)) break;
+		/*get the next byte as end byte*/
 		if(!index_get(win.index, &end, i)) break;
 
+		/*read the file between start, end*/
 		win_read_file(buf, start, end);
+		/*discard \r if exists*/
 		buf[strcspn(buf, "\r")] = '\0';
+		/*draw the newly received line buffer on screen*/
 		win_draw_line(buf);
-	}
+	} /*while(i < -1)*/
 
+	/*save the last display bytes*/
 	index_get(win.index, &(win.display_bytes), -1);
+	/*position the file pointer at the last read byte*/
 	fseek(win.fd, win.read_bytes, SEEK_SET);
-	wrefresh(win.win); //???
+	/*draw from virtual screen to user screen*/
+	wrefresh(win.win);
 } /*win_draw()*/
 
 
+/*
+ * Indexes the irclsd output log file into the win.index object
+ */
 static void
 win_index_file(void)
 {
-	int ch;
+	int ch;                                  //one character
 
-	while((ch = fgetc(win.fd)) != EOF)
+	while((ch = fgetc(win.fd)) != EOF)       //get ch until EOF
 	{
-		win.read_bytes++;
+		win.read_bytes++;                //increment the already read
+
+		/*we are indexing the newlines in the file by adding them to
+		 * the win.index object*/
 		if(ch == '\n')	index_append(win.index, win.read_bytes);
 	}
 } /*win_index_file()*/
 
 
+/*
+ * Reads irclsd output log file between start, end bytes
+ * buf: buffer to read into
+ */
 static void
 win_read_file(char buf[CBUFLEN + 1], const size_t start, const size_t end)
 {
-	size_t read_size = end - start;
-	read_size = MIN(read_size, CBUFLEN + 1);
+	size_t read_size = end - start;          //how many bytes to read
+	read_size = MIN(read_size, CBUFLEN + 1); //buffer overflow protection
 
+	/*go to start*/
 	if(fseek(win.fd, start, SEEK_SET) != 0) return;
 
-	fread(buf, sizeof(char), read_size, win.fd); /*how to check success?*/
+	fread(buf, sizeof(char), read_size, win.fd);
+	/*FIXME: here may have fread success check protection*/
 	buf[read_size - 1] = '\0';
 } /*win_read_file()*/
 
 
+/*
+ * Draws one line from the given char buffer
+ * buf: character buffer to draw on screen
+ */
 static void
 win_draw_line(const char buf[CBUFLEN + 1])
 {
-	int res = ircproto_parse(win.ircproto, buf);
+	int res = ircproto_parse(win.ircproto, buf); //parse irc protocol
 
+	/* act according to the result from ircproto_parse()*/
 	if(res == IRC_RES_PRIVMSG) win_draw_line_privmsg();
-	else if((res == IRC_RES_UNKNOWN) &&
-		(win.ircproto->mark == L'>') &&
-		(wcsncmp(win.ircproto->params, L"PONG", 4) != 0))
-		win_draw_line_myunspecified();
+	else if((res == IRC_RES_UNKNOWN) &&                      //unknown line
+		(win.ircproto->mark == L'>') &&                  //from me
+		(wcsncmp(win.ircproto->params, L"PONG", 4) != 0))//not PONG
+		win_draw_line_myunspecified();                   //is drawn
 	else if(res == IRC_RES_MYPRIVMSG) win_draw_line_myprivmsg();
 	else if(res == IRC_RES_ERROR) win_draw_line_error();
 } /*win_draw_line()*/
 
 
+/*
+ * Draws a privmsg line on screen
+ */
 static void
 win_draw_line_privmsg(void)
 {
-	int h;
-	int w;
-	int flag_tome = 0;
+	int h;                                   //height
+	int w;                                   //width
+	int flag_tome = 0;                       //is privmsg send to me?
 
-	if((tosend[0] != '\0') &&
-	   (wcsncmp(win.ircproto->tonick, tosend, CHANLEN) != 0) &&
-	   (wcsncmp(win.ircproto->tonick, mynick, NICKLEN != 0)))
-		return;
+	/*if 'tosend' is not empty, filter all other messages except messages
+	 * sent directly to me*/
+	if((tosend[0] != '\0') &&                                   //nonempty
+	   (wcsncmp(win.ircproto->tonick, tosend, CHANLEN) != 0) && //no tosend
+	   (wcsncmp(win.ircproto->tonick, mynick, NICKLEN != 0)))   //not to me
+		return;                                             //bye
 
 	getmaxyx(win.win, h, w);
-	wmove(win.win, h - 1, 0);
+	wmove(win.win, h - 1, 0);                //move to the begining
+	                                         //of the last row
 
+	/*draw short version of time*/
 	wattron(win.win, style.time);
 	wprintw(win.win, "%ls ", win.ircproto->tshort);
 	wattroff(win.win, style.time);
 
+	/*bring up flag_tome if the message is sent to me*/
 	if(wcsncmp(win.ircproto->tonick, mynick, NICKLEN) == 0)
 	{
 		flag_tome = 1;
 		count_tome++;
 	}
 
+	/*arrange style*/
 	if(flag_tome) wattron(win.win, style.priv);
+	/*this below arranges rotational style for the given tonick*/
 	else wattron(win.win, stylerot_get(win.ircproto->tonick));
 
+	/*draw tonick and nick*/
 	wprintw(win.win, "[%ls] ", win.ircproto->tonick);
 	wattron(win.win, style.mod_nick);
 	wprintw(win.win, "<%ls>", win.ircproto->nick);
 	wattroff(win.win, style.mod_nick);
 
+	/*off style*/
 	if(!flag_tome) wattroff(win.win, stylerot_get(win.ircproto->tonick));
 
+	/*draw the message*/
 	wprintw(win.win, " %ls\n", win.ircproto->params);
 
+	/*off style*/
 	if(flag_tome) wattroff(win.win, style.priv);
-
 } /*win_draw_line_privmsg()*/
 
 
+/* 
+ * Draws my privmsg line (privmsg sent from me) on screen
+ */
 static void
 win_draw_line_myprivmsg(void)
 {
-	int h;
-	int w;
+	int h;                                   //height
+	int w;                                   //width
 
 	getmaxyx(win.win, h, w);
-	wmove(win.win, h - 1, 0);
+	wmove(win.win, h - 1, 0);                //beginning of last line
 
+	/*draw time*/
 	wattron(win.win, style.time);
 	wprintw(win.win, "%ls", win.ircproto->tshort);
 	wattroff(win.win, style.time);
 
-	wattron(win.win, style.priv);
+	wattron(win.win, style.priv);            //style is private style
+
+	/*draw tonick, usually this is a channel*/
 	wprintw(win.win, " [%ls] ", win.ircproto->tonick);
 
+	/*draw sending nick (this is me)*/
 	wattron(win.win, style.mod_nick);
 	wprintw(win.win, "<%ls> ", win.ircproto->nick);
 	wattroff(win.win, style.mod_nick);
 
+	/*draw the message*/
 	wprintw(win.win, "%ls\n", win.ircproto->params);
-	wattron(win.win, style.priv);
+
+	wattron(win.win, style.priv);            //end style
 } /*win_draw_line_myprivmsg()*/
 
 
+/*
+ * Draws an error line on screen
+ */
 static void
 win_draw_line_error(void)
 {
-	int h;
-	int w;
+	int h;                                   //height
+	int w;                                   //width
 
 	getmaxyx(win.win, h, w);
-	wmove(win.win, h - 1, 0);
+	wmove(win.win, h - 1, 0);                //beginning of last line
 
+	/*draw*/
 	wattron(win.win, style.error);
 	wprintw(win.win, "%ls %lc %ls\n",
 		win.ircproto->t,
@@ -240,47 +327,56 @@ win_draw_line_error(void)
 } /*win_draw_line_error()*/
 
 
+/*
+ * Draws one line on screen of unspecified type sent from me
+ */
 static void
 win_draw_line_myunspecified(void)
 {
-	int h;
-	int w;
+	int h;                                   //height
+	int w;                                   //width
 
 	getmaxyx(win.win, h, w);
-	wmove(win.win, h - 1, 0);
+	wmove(win.win, h - 1, 0);                //to beginning of last line
 
-	wattron(win.win, style.time);
+	wattron(win.win, style.time);            //draw time
 	wprintw(win.win, "%ls", win.ircproto->tshort);
 	wattroff(win.win, style.time);
 
-	wattron(win.win, style.priv);
+	wattron(win.win, style.priv);            //draw nick (this is me)
 	wattron(win.win, style.mod_nick);
 	wprintw(win.win, " <%ls> ", mynick);
 	wattroff(win.win, style.mod_nick);
 
-	wprintw(win.win, "%ls\n", win.ircproto->params);
+	wprintw(win.win, "%ls\n", win.ircproto->params); //the raw IRC command
 	wattron(win.win, style.priv);
 } /*win_draw_line_myunspecified()*/
 
 
+/*
+ * Clears screen in funny way
+ */
 void 
 win_matrix_reloaded(void)
 {
-	int h;
-	int w;
-	int x;
-	int y;
-	int chcount;
-	int styl;
-	struct timeval tv;
+	int h;                                   //height
+	int w;                                   //width
+	int x;                                   //screen position x
+	int y;                                   //screen position y
+	int chcount;                             //character count
+	int styl;                                //ncurses style
+	struct timeval tv;                       //needed for microsleep
 
 	getmaxyx(win.win, h, w);
 
-	chcount = h * w;
+	chcount = h * w;                         //all characters on screen
 
 	curs_set(0);                             //cursor off (more beautyful)
+
 	wmove(win.win, 0, 0);
-	srand(time(0)); //!!!
+
+	srand(time(0));                          //needed for stylerot_random()
+	                                         //and range_random()
 	for(int i = 0; i < chcount; i++)
 	{
 
@@ -297,7 +393,7 @@ win_matrix_reloaded(void)
 
 
 	}
-	wrefresh(win.win);                       //display all on scr
+	wrefresh(win.win);                       //display all on screen now
 
 	for(int i = 0; i < chcount; i++)
 	{
--- src/win/win.h
+++ src/win/win.h
@@ -1,62 +1,111 @@
+/*
+ * The ncurses chat window object
+ * Reads its data from the irclsd output log file and displays it
+ * on the screen in 'tail -f' fashion.
+ * There is no buffer history, the 'part', 'join', 'quit' messages are not
+ * displayed. Only privmsg are displayed.
+ * Feature: when 'To:' is sent by user, all other messages which do not
+ * corespond to 'To:' are filtered
+ */
 #ifndef WIN_H
 #define WIN_H
 
 
-#include <global/global.h>
-#include <index/index.h>
-#include <ircproto/ircproto.h>
+#include <global/global.h>                       //PATHLEN
+#include <index/index.h>                         //index object
+#include <ircproto/ircproto.h>                   //ircproto object
 
-#include <ncursesw/curses.h> //WINDOW
-#include <panel.h> //PANEL
-#include <stdio.h> //FILE
-#include <wchar.h> //wchar_t
-
-
-#define HISTORYBUF 100   //must be greater than window height, otherwise loss
+#include <ncursesw/curses.h>                     //WINDOW
+#include <panel.h>                               //PANEL
+#include <stdio.h>                               //FILE
+#include <wchar.h>                               //wchar_t
 
 
+/* how many of the trailing lines of irclsd output log file to keep indexed?
+ * this needs to be more than the terminal LINES */
+#define HISTORYBUF 100
+
+
+/*data structure for the chat window object*/
 typedef struct Win
 {
-	FILE *fd;
-	WINDOW *win;
-	PANEL *pan;
-	Index *index;
-	size_t read_bytes;
-	size_t display_bytes;
-	IrcProto *ircproto;
+	FILE *fd;                                //file descriptor irclsd log
+	WINDOW *win;                             //ncurses window
+	PANEL *pan;                              //ncurses panel
+	Index *index;                            //indexing object
+	size_t read_bytes;                       //how many bytes read from log
+	size_t display_bytes;                    //how many bytes displayed
+	IrcProto *ircproto;                      //IRC protocol parser
 } Win;
 
 
+/*
+ * Initializes the chat window object
+ * filename: the filename of the irclsd output log file
+ * Returns: 0 OK, negative ERR
+ */
 int
 win_init(const char filename[PATHLEN + 1]);
 
+/*
+ * Frees memory and pointers
+ */
 void
 win_del(void);
 
+/*
+ * Draws the window on the screen
+ */
 void
 win_draw(void);
 
+/*
+ * Indexes the irclsd output log file into the win.index object
+ */
 static void
 win_index_file(void);
 
+/*
+ * Reads irclsd output log file between start, end bytes
+ * buf: buffer to read into
+ */
 static void
 win_read_file(char buf[CBUFLEN + 1], const size_t start, const size_t end);
 
+/*
+ * Draws one line from the given char buffer
+ * buf: character buffer to draw on screen
+ */
 static void
 win_draw_line(const char buf[CBUFLEN + 1]);
 
+/*
+ * Draws a privmsg line on screen
+ */
 static void
 win_draw_line_privmsg(void);
 
+/* 
+ * Draws my privmsg line (privmsg sent from me) on screen
+ */
 static void
 win_draw_line_myprivmsg(void);
 
+/*
+ * Draws an error line on screen
+ */
 static void
 win_draw_line_error(void);
 
+/*
+ * Draws one line on screen of unspecified type sent from me
+ */
 static void
 win_draw_line_myunspecified(void);
 
+/*
+ * Clears screen in funny way
+ */
 void
 win_matrix_reloaded(void);
 
